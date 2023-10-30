b5fcc56b595c9cbc0e400f52b861ea8e3a2d3707
durga0702 <durga@sparkouttech.com>
2023-10-30 12:33:28 +0530

    library works

---
+++ projects/weather/src/lib/weather.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-weather',
+  template: `
+    <p>
+      weather works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class WeatherComponent {
+
+}
---
+++ projects/weather/src/lib/weather.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { WeatherComponent } from './weather.component';
+
+
+
+@NgModule({
+  declarations: [
+    WeatherComponent
+  ],
+  imports: [
+  ],
+  exports: [
+    WeatherComponent
+  ]
+})
+export class WeatherModule { }
